dcac04ea34a1c466e63d28217c0fad13daa42c83
Alexandra Grasland <alexgrasland@gmail.com>
2019-01-18 16:00:26 +0100

    feat(UI): add custom favicons for staff/yolo versions

--- server/handler.go
+++ server/handler.go
@@ -228,7 +228,16 @@ func (s *Server) ListReleaseIOSBeta(c echo.Context) error {
 }
 
 func (s *Server) ListRelease(c echo.Context, job string) error {
-	html := `<html><head><meta http-equiv="refresh" content="30"><link rel="stylesheet" href="/assets/site.css">` + faviconHTMLHeader + `</head><body><div class="container">`
+	pageKind := "default"
+	switch job {
+		case IOS_JOB, ANDROID_JOB:
+			pageKind = "staff"
+		case IOS_HOUSE_JOB:
+			pageKind = "yolo"
+	}
+	faviconHTMLHeader := `<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/` + pageKind + `/apple-touch-icon.png + "><link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/` + pageKind + `/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/` + pageKind + `/favicon-16x16.png"><link rel="manifest" href="/assets/favicon/` + pageKind + `/site.webmanifest"><link rel="mask-icon" href="/assets/favicon/` + pageKind + `/safari-pinned-tab.svg" color="#5bbad5"><meta name="msapplication-TileColor" content="#262844"><meta name="theme-color" content="#ffffff">`
+
+	html := `<html><head><title>` + pageKind + ` - Berty</title><meta http-equiv="refresh" content="30"><link rel="stylesheet" href="/assets/site.css">` + faviconHTMLHeader + `</head><body><div class="container">`
 
 	dlIcon := `<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>`
 
--- server/server.go
+++ server/server.go
@@ -81,14 +81,6 @@ func randStringRunes(n int) string {
 	return string(b)
 }
 
-var faviconHTMLHeader = `<link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
-<link rel="manifest" href="/assets/site.webmanifest">
-<link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#262844">
-<meta name="theme-color" content="#ffffff">`
-
 func NewServer(cfg *ServerConfig) *Server {
 	randStr := randStringRunes(10)
 	e := echo.New()
