fda19cd56df13929d05307425a1c7fd9488fa5f0
Elizabeth Kelen <erskelen@gmail.com>
2020-04-09 00:43:52 +0200

    feat(web): add spec styles and icons AND proof of concept for light and dark theme toggler
    chore: add color variables from v2 spec
    
    feat(web): Add theme toggler
    
    fix: override tabler button styles
    
    build: add svgs
    
    test: add mock data file
    
    build: install icon libraries

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,44 @@
 /* eslint-disable import/no-named-as-default */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {hot} from 'react-hot-loader';
 import Home from './ui/pages/Home';
 
 // Import Tabler styles
 import 'tabler/scss/tabler.scss';
-
 import './App.scss';
 
+const detectBrowserTheme = () => {
+  const supportsPreference =
+    window.matchMedia('(prefers-color-scheme)').media !== 'not all';
+  const isDark =
+    supportsPreference &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
+  return {isDark};
+};
+
+// TODO: Inject theme from Context
 const App = () => {
+  const [themeIsDark, setIsDark] = useState(false);
+  const themeName = () => (themeIsDark ? 'theme-dark' : 'theme-light');
+
+  useEffect(() => {
+    const {isDark} = detectBrowserTheme();
+    if (isDark) setIsDark(true);
+  }, []);
+
+  const toggleTheme = () => {
+    setIsDark(!themeIsDark);
+  };
+
   return (
-    <div className="page">
+    <div className={'page ' + themeName()}>
       <Home />
+      <div className="footer p-4">
+        <p>Yolo Footer</p>
+        <div className="btn btn-primary" onClick={toggleTheme}>
+          Toggle Theme
+        </div>
+      </div>
     </div>
   );
 };
--- web/src/ui/components/BuildList.js
+++ web/src/ui/components/BuildList.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Card from './Card';
 import axios from 'axios';
+import {results} from '../../assets/sample-build-response';
 
 const BuildList = ({platformName, platformId}) => {
   const [error, setError] = useState(null);
@@ -10,33 +11,40 @@ const BuildList = ({platformName, platformId}) => {
 
   // TODO: Move these controllers out of component code
   useEffect(() => {
-    const getBuilds = () =>
-      axios
-        .get(`${process.env.API_URL}/build-list?artifact_kind=${platformId}&`, {
-          headers: process.env.YOLO_APP_PW
-            ? {
-                Authorization: 'Basic ' + btoa(`${process.env.YOLO_APP_PW}`),
-              }
-            : {},
-        })
-        .then(
-          (result) => {
-            const {
-              data: {builds},
-            } = result;
-            setIsLoaded(true);
-            setBaseURL(
-              `${document.location.protocol}//${document.location.host}`
-            );
-            setItems(builds);
-          },
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        );
+    console.log(`process.env.YOLO_UI_TEST:`, process.env.YOLO_UI_TEST);
+    const source = () =>
+      process.env.YOLO_UI_TEST === 'true'
+        ? Promise.resolve(results)
+        : axios.get(
+            `${process.env.API_URL}/build-list?artifact_kind=${platformId}&`,
+            {
+              headers: process.env.YOLO_APP_PW
+                ? {
+                    Authorization:
+                      'Basic ' + btoa(`${process.env.YOLO_APP_PW}`),
+                  }
+                : {},
+            }
+          );
+    const getBuilds = (getFunction = () => null) =>
+      getFunction().then(
+        (result) => {
+          const {
+            data: {builds},
+          } = result;
+          setIsLoaded(true);
+          setBaseURL(
+            `${document.location.protocol}//${document.location.host}`
+          );
+          setItems(builds);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
     if (!isLoaded) {
-      getBuilds();
+      getBuilds(source);
     }
   });
 
