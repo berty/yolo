024147d5ddf8b41b99f8d5ca656b785f1977bfe0
Elizabeth Kelen <erskelen@gmail.com>
2020-05-11 19:54:15 +0200

    fix(front): fix unwanted theme change on opening filter settings modal
    remove debug border from filters modal
    
    move theme detector from component to store

--- web/src/store/ThemeStore.js
+++ web/src/store/ThemeStore.js
@@ -1,13 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {themes} from '../ui/styleTools/themes';
 
 export const ThemeContext = React.createContext();
 
+const detectBrowserTheme = () => {
+  const supportsPreference =
+    window.matchMedia('(prefers-color-scheme)').media !== 'not all';
+  const isLight =
+    supportsPreference &&
+    window.matchMedia('(prefers-color-scheme: light)').matches;
+  return {isLight};
+};
+
 export const ThemeStore = ({children}) => {
   const [theme, setTheme] = useState(themes.dark);
+
   const changeTheme = (newName) => {
     return setTheme(themes[newName] || themes.dark);
   };
+
+  useEffect(() => {
+    const {isLight} = detectBrowserTheme();
+    if (isLight) changeTheme('light');
+  }, []);
+
   return (
     <ThemeContext.Provider value={{theme, changeTheme}}>
       {children}
--- web/src/ui/components/ThemeToggler.js
+++ web/src/ui/components/ThemeToggler.js
@@ -1,22 +1,8 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import {ThemeContext} from '../../store/ThemeStore';
-
-const detectBrowserTheme = () => {
-  const supportsPreference =
-    window.matchMedia('(prefers-color-scheme)').media !== 'not all';
-  const isLight =
-    supportsPreference &&
-    window.matchMedia('(prefers-color-scheme: light)').matches;
-  return {isLight};
-};
 
 const ThemeToggler = () => {
   const {theme, changeTheme} = useContext(ThemeContext);
-
-  useEffect(() => {
-    const {isLight} = detectBrowserTheme();
-    if (isLight) changeTheme('light');
-  }, []);
 
   return (
     <div
