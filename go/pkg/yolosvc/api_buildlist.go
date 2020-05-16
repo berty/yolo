7883ee68497c85e0ee1f1f96d89cd47bf772d103
Manfred Touron <94029+moul@users.noreply.github.com>
2020-05-16 22:46:10 +0200

    fix: support artifact_kinds and build_id filters together

--- go/pkg/yolosvc/api_buildlist.go
+++ go/pkg/yolosvc/api_buildlist.go
@@ -20,49 +20,44 @@ func (svc service) BuildList(ctx context.Context, req *yolopb.BuildList_Request)
 
 	resp := yolopb.BuildList_Response{}
 
-	query := svc.db.
-		Model(&yolopb.Build{})
+	query := svc.db.Model(&yolopb.Build{})
+	noMoreFilters := false
 
 	switch {
-	case len(req.BuildID) > 0:
-		query = query.
-			Where("build.id IN (?) OR build.yolo_id IN (?)", req.BuildID, req.BuildID).
-			Preload("HasArtifacts")
 	case len(req.ArtifactID) > 0:
 		query = query.
 			Joins("JOIN artifact ON artifact.has_build_id = build.id AND (artifact.id IN (?) OR artifact.yolo_id IN (?))", req.ArtifactID, req.ArtifactID).
 			Preload("HasArtifacts")
+		noMoreFilters = true
+	case len(req.ArtifactKinds) > 0:
+		query = query.
+			Joins("JOIN artifact ON artifact.has_build_id = build.id AND artifact.kind IN (?)", req.ArtifactKinds).
+			Preload("HasArtifacts", "kind IN (?)", req.ArtifactKinds)
+	case req.WithArtifacts:
+		query = query.
+			Joins("JOIN artifact ON artifact.has_build_id = build.id", req.ArtifactKinds).
+			Preload("HasArtifacts")
 	default:
-		switch {
-		case len(req.ArtifactKinds) > 0:
-			query = query.
-				Joins("JOIN artifact ON artifact.has_build_id = build.id AND artifact.kind IN (?)", req.ArtifactKinds).
-				Preload("HasArtifacts", "kind IN (?)", req.ArtifactKinds)
-		case req.WithArtifacts:
-			query = query.
-				Joins("JOIN artifact ON artifact.has_build_id = build.id", req.ArtifactKinds).
-				Preload("HasArtifacts")
-		default:
-			query = query.
-				Preload("HasArtifacts")
-		}
+		query = query.
+			Preload("HasArtifacts")
+	}
 
+	if !noMoreFilters {
+		if len(req.BuildID) > 0 {
+			query = query.Where("build.id IN (?) OR build.yolo_id IN (?)", req.BuildID, req.BuildID)
+		}
 		if len(req.BuildState) > 0 {
 			query = query.Where("build.state IN (?)", req.BuildState)
 		}
-
 		if len(req.BuildDriver) > 0 {
 			query = query.Where("build.driver IN (?)", req.BuildDriver)
 		}
-
 		if len(req.ProjectID) > 0 {
 			query = query.Joins("JOIN project ON project.id = build.has_project_id AND (project.id IN (?) OR project.yolo_id IN (?))", req.ProjectID, req.ProjectID)
 		}
-
 		if len(req.MergeRequestID) > 0 {
 			query = query.Where("build.has_mergerequest_id IN (?)", req.MergeRequestID)
 		}
-
 		if len(req.MergeRequestAuthorID) > 0 ||
 			len(req.MergerequestState) > 0 ||
 			req.WithMergerequest {
