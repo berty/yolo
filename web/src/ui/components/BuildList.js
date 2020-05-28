22690a011f67c548f28abce4a800070c21d97ffc
Elizabeth Kelen <erskelen@gmail.com>
2020-05-28 23:53:53 +0200

    memoize check for master branch in buildlist

--- web/src/ui/components/BuildList.js
+++ web/src/ui/components/BuildList.js
@@ -13,33 +13,29 @@ import Divider from './Divider/Divider'
 import { getDayFormat } from '../../util/date'
 
 const BuildList = ({ builds = [] }) => {
-<<<<<<< HEAD
   const oneBuildInResultsHasMaster = useMemo(() => oneBuildResultHasBranchMaster(builds), [builds])
-=======
-  const oneBuildInResultsHasMaster = oneBuildResultHasBranchMaster(builds)
->>>>>>> split out and memoize group builds by MR
   const buildsByMr = useMemo(() => flagBuildsFirstOfDay(groupBuildsByMr(builds)), [builds])
   const NoBuilds = () => <div>No results match your query.</div>
 
   return !builds.length ? (
     <NoBuilds />
   ) : (
-    <div className="container">
-      {buildsByMr.map((build, i) => (
-        <BuildContainer
-          key={`${build.id}-${i}`}
-          build={build}
-          toCollapse={oneBuildInResultsHasMaster && !buildBranchIsMaster(build)}
-          hasRunningBuilds={buildsStateIsRunning(build.allBuildsForMr, builds)}
-        >
-          {build.buildIsFirstOfDay && (
-          <Divider dividerText={getDayFormat(build.created_at)} />
-          )}
+      <div className="container">
+        {buildsByMr.map((build, i) => (
+          <BuildContainer
+            key={`${build.id}-${i}`}
+            build={build}
+            toCollapse={oneBuildInResultsHasMaster && !buildBranchIsMaster(build)}
+            hasRunningBuilds={buildsStateIsRunning(build.allBuildsForMr, builds)}
+          >
+            {build.buildIsFirstOfDay && (
+              <Divider dividerText={getDayFormat(build.created_at)} />
+            )}
 
-        </BuildContainer>
-      ))}
-    </div>
-  )
+          </BuildContainer>
+        ))}
+      </div>
+    )
 }
 
 export default BuildList
