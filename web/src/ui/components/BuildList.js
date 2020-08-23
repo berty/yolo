f010fb86f09080b39b84539d80f0cfe57c1f73b1
Elizabeth Kelen <erskelen@gmail.com>
2020-08-23 10:10:53 -0400

    chore: lint
    Signed-off-by: Elizabeth Kelen <erskelen@gmail.com>

--- web/src/ui/components/BuildList.js
+++ web/src/ui/components/BuildList.js
@@ -1,40 +1,39 @@
-import React, { Fragment, useMemo } from "react";
+import React, { Fragment, useMemo } from 'react'
 import {
   buildsStateIsRunning,
   oneBuildResultHasBranchMaster,
-} from "../../api/apiResponseGetters";
+} from '../../api/apiResponseGetters'
 import {
   flagBuildsFirstOfDay,
   getLatestMasterBuildsForProjects,
   groupBuildsByMr,
-} from "../../api/apiResponseTransforms";
-import { getDayFormat } from "../../util/date";
-import { getIsArrayWithN } from "../../util/getters";
-import BuildContainer from "./Build/BuildContainer";
-import Divider from "./Divider/Divider";
+} from '../../api/apiResponseTransforms'
+import { getDayFormat } from '../../util/date'
+import { getIsArrayWithN } from '../../util/getters'
+import BuildContainer from './Build/BuildContainer'
+import Divider from './Divider/Divider'
 
 const BuildList = React.memo(({ builds = [], loaded }) => {
   const oneBuildInResultsHasMaster = useMemo(
     () => oneBuildResultHasBranchMaster(builds),
-    [builds]
-  );
-  const buildsByMr = useMemo(() => groupBuildsByMr(builds), [builds]);
+    [builds],
+  )
+  const buildsByMr = useMemo(() => groupBuildsByMr(builds), [builds])
   const buildsByMrWithDateFlags = useMemo(
     () => flagBuildsFirstOfDay(buildsByMr),
-    [buildsByMr]
-  );
-  const indexOfLatestMasterBuildsForProjects =
-    !getIsArrayWithN(buildsByMrWithDateFlags, 2) || !oneBuildInResultsHasMaster
-      ? []
-      : getLatestMasterBuildsForProjects(buildsByMrWithDateFlags);
-  const NoBuilds = () => <div>No results match your query.</div>;
+    [buildsByMr],
+  )
+  const indexOfLatestMasterBuildsForProjects = !getIsArrayWithN(buildsByMrWithDateFlags, 2) || !oneBuildInResultsHasMaster
+    ? []
+    : getLatestMasterBuildsForProjects(buildsByMrWithDateFlags)
+  const NoBuilds = () => <div>No results match your query.</div>
 
   return !builds.length && loaded ? (
     <NoBuilds />
   ) : (
     <div className="container">
-      {indexOfLatestMasterBuildsForProjects.length > 0 &&
-        buildsByMrWithDateFlags
+      {indexOfLatestMasterBuildsForProjects.length > 0
+        && buildsByMrWithDateFlags
           .filter((_, i) => indexOfLatestMasterBuildsForProjects.includes(i))
           .map((build, i) => (
             <BuildContainer
@@ -44,35 +43,35 @@ const BuildList = React.memo(({ builds = [], loaded }) => {
               isLatestMaster
               hasRunningBuilds={buildsStateIsRunning(
                 build.allBuildsForMr,
-                builds
+                builds,
               )}
             />
           ))}
       {buildsByMrWithDateFlags.map((
         build,
-        i // The i is important to get day dividers, so don't change length of this array
+        i, // The i is important to get day dividers, so don't change length of this array
       ) => (
         <Fragment key={`${build.id}-${i}`}>
-          {build.buildIsFirstOfDay &&
-            !indexOfLatestMasterBuildsForProjects.includes(i) && (
+          {build.buildIsFirstOfDay
+            && !indexOfLatestMasterBuildsForProjects.includes(i) && (
               <Divider dividerText={getDayFormat(build.created_at)} />
-            )}
+          )}
           {!indexOfLatestMasterBuildsForProjects.includes(i) && (
             <BuildContainer
               build={build}
               toCollapse={indexOfLatestMasterBuildsForProjects.length > 0}
               hasRunningBuilds={buildsStateIsRunning(
                 build.allBuildsForMr,
-                builds
+                builds,
               )}
             />
           )}
         </Fragment>
       ))}
     </div>
-  );
-});
+  )
+})
 
 // BuildList.whyDidYouRender = true
 
-export default BuildList;
+export default BuildList
