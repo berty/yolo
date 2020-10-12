237b097dfb84c66436318ec4a478a98e6f674375
E Kelen <erskelen@gmail.com>
2020-10-12 15:08:24 -0400

    feat(js): scroll-to on feed len change (#403)
    Signed-off-by: Elizabeth Kelen <erskelen@gmail.com>

--- web/src/util/misc.js
+++ web/src/util/misc.js
@@ -1,1 +1,11 @@
+import { useEffect, useRef } from 'react'
+
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export function usePrevious(value) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
