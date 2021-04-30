93c4252f3ceab18b53c230d9f9c63666453cf1fe
Darko Djalevski <dare_zalevski@yahoo.com>
2021-04-30 18:26:35 +0200

    refactor: added some storage methods for artifacts,builds,api_dev
    Signed-off-by: Darko Djalevski <dare_zalevski@yahoo.com>

--- go/pkg/yolostore/gorm.go
+++ go/pkg/yolostore/gorm.go
@@ -1,8 +1,18 @@
 package yolostore
 
-import "github.com/jinzhu/gorm"
+import (
+	"fmt"
+
+	"berty.tech/yolo/v2/go/pkg/yolopb"
+	"github.com/jinzhu/gorm"
+)
 
 type Store interface {
+	GetArtifactByID(id string) (*yolopb.Artifact, error)
+	GetBuildListFilters() (*yolopb.BuildListFilters_Response, error)
+	GetBatchWithPreloading() (*yolopb.Batch, error)
+	GetBatch() (*yolopb.Batch, error)
+	GetDevDumpObjectDownloads() ([]*yolopb.Download, error)
 }
 
 type store struct {
@@ -13,4 +23,144 @@ func NewStore(db *gorm.DB) Store {
 	return &store{
 		db: db,
 	}
+}
+
+func (s *store) GetArtifactByID(id string) (*yolopb.Artifact, error) {
+	var artifact *yolopb.Artifact
+	err := s.db.
+		Preload("HasBuild").
+		Preload("HasBuild.HasProject").
+		Preload("HasBuild.HasProject.HasOwner").
+		First(&artifact, "ID = ?", id).
+		Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetArtifactByID :%w", err)
+	}
+
+	return artifact, nil
+}
+
+func (s *store) GetBuildListFilters() (*yolopb.BuildListFilters_Response, error) {
+	blFilters := yolopb.BuildListFilters_Response{}
+
+	err := s.db.Find(&blFilters.Entities).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBuildListFilters: find: %w", err)
+	}
+
+	err = s.db.Preload("HasOwner").Find(&blFilters.Projects).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBuildListFilters: preload has owner: %w", err)
+	}
+
+	return &blFilters, nil
+}
+
+func (s *store) GetBatchWithPreloading() (*yolopb.Batch, error) {
+	batch := yolopb.NewBatch()
+
+	err := s.db.
+		Preload("HasOwner").
+		Find(&batch.Projects).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatchWithPreloading: preload HasOwner: %w", err)
+	}
+	err = s.db.
+		// FIXME: TODO
+		Find(&batch.Entities).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatchWithPreloading: find entities: %w", err)
+	}
+	err = s.db.
+		Preload("HasCommit").
+		Preload("HasProject").
+		Preload("HasMergerequest").
+		Preload("HasMergerequest.HasProject").
+		Preload("HasMergerequest.HasProject.HasOwner").
+		Preload("HasMergerequest.HasAuthor").
+		Preload("HasMergerequest.HasCommit").
+		Find(&batch.Builds).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatchWithPreloading: preload HasCommit,HasProject,HasMergerequest: %w", err)
+	}
+	err = s.db.
+		Preload("HasBuild").
+		Preload("HasBuild.HasProject").
+		Preload("HasBuild.HasMergerequest").
+		Preload("HasBuild.HasMergerequest.HasProject").
+		Preload("HasBuild.HasMergerequest.HasAuthor").
+		Preload("HasBuild.HasMergerequest.HasCommit").
+		Preload("HasBuild.HasCommit").
+		Find(&batch.Artifacts).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatchWithPreloading: preload HasBuild: %w", err)
+	}
+	err = s.db.
+		Preload("HasCommit").
+		Preload("HasProject").
+		Preload("HasAuthor").
+		Find(&batch.MergeRequests).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatchWithPreloading: MergeRequest: %w", err)
+	}
+	err = s.db.
+		// FIXME: TODO
+		Find(&batch.Releases).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatchWithPreloading: find Relases: %w", err)
+	}
+	err = s.db.
+		// FIXME: TODO
+		Find(&batch.Commits).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatchWithPreloading: find Commits: %w", err)
+	}
+
+	return batch, nil
+}
+
+func (s *store) GetBatch() (*yolopb.Batch, error) {
+	batch := yolopb.NewBatch()
+
+	err := s.db.Find(&batch.Projects).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatch: find Projects :%w", err)
+	}
+	err = s.db.Find(&batch.Entities).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatch: find Entities :%w", err)
+	}
+	err = s.db.Find(&batch.Builds).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatch: find Builds :%w", err)
+	}
+	err = s.db.Find(&batch.Artifacts).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatch: find Artifacts :%w", err)
+	}
+	err = s.db.Find(&batch.MergeRequests).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatch: find MergeRequests :%w", err)
+	}
+	err = s.db.Find(&batch.Releases).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatch: find Relases :%w", err)
+	}
+	err = s.db.Find(&batch.Commits).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetBatch: find Commits :%w", err)
+	}
+
+	return batch, nil
+}
+
+func (s *store) GetDevDumpObjectDownloads() ([]*yolopb.Download, error) {
+	resp := yolopb.DevDumpObjects_Response{}
+
+	err := s.db.Find(&resp.Downloads).Error
+	if err != nil {
+		return nil, fmt.Errorf("store: GetDevDumpObjectDownloads: find Downloads: %w", err)
+	}
+
+	return resp.Downloads, nil
 }
--- go/pkg/yolosvc/api_buildlistfilters.go
+++ go/pkg/yolosvc/api_buildlistfilters.go
@@ -7,21 +7,14 @@ import (
 )
 
 func (svc *service) BuildListFilters(ctx context.Context, req *yolopb.BuildListFilters_Request) (*yolopb.BuildListFilters_Response, error) {
-	resp := yolopb.BuildListFilters_Response{}
-
 	// FIXME: limit most recent entities
 	// FIXME: sort entries by "popularity" or "freshness"
 	// FIXME: add available branches
 
-	err := svc.db.Find(&resp.Entities).Error
+	resp, err := svc.store.GetBuildListFilters()
 	if err != nil {
 		return nil, err
 	}
 
-	err = svc.db.Preload("HasOwner").Find(&resp.Projects).Error
-	if err != nil {
-		return nil, err
-	}
-
-	return &resp, nil
+	return resp, nil
 }
